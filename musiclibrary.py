06de669d6dfb6d7f561e0b8edeeafffcb7498d45
Saurav Kumar <ssauravkr1111@gmail.com>
2026-06-29 18:17:28 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "millionaire" : "https://open.spotify.com/track/78BWCd70D1X6LMkDZm1UoF?si=c9e5b0a3a13c4c7c",
+    "love dose" : "https://open.spotify.com/track/1QZRCi2Z1DQQaR6bGAzhtN?si=893c3fb7e5604e58",
+    "blue eyes" : "https://open.spotify.com/album/2iPimv2hsVBJZU1fvBfwkB?si=iRExX2_CQ0O8PxqCIM5gCg",
+    "payal" : "https://open.spotify.com/track/76ZWOhRRQzmb4xMoZzTjJ9?si=ef3af0ee74244581"
+}
