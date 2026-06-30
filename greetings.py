d13cd1b1a4b5857688d2059291de0b94ea61cfdb
Saurav Kumar <ssauravkr1111@gmail.com>
2026-06-30 11:08:03 +0530

    Some Greeting instructions

---
+++ greetings.py
@@ -0,0 +1,14 @@
+greetings = {
+    "": "audios/yes.mp3",
+    "hi": "audios/hi.mp3",
+    "hello": "audios/hello.mp3",
+    "hey": "audios/hey.mp3",
+    "hay": "audios/hey.mp3",
+    "good morning": "audios/good_morning.mp3",
+    "good afternoon": "audios/good_afternoon.mp3",
+    "good evening": "audios/good_evening.mp3",
+    "namaste": "audios/namaste.mp3",
+    "namaskar": "audios/namaskar.mp3",
+    "how are you": "audios/how_are_you.mp3",
+    "how r u": "audios/how_are_you.mp3",
+}
