cb501538fb5a23ccf2e2868721ecefde95bf765d
Saurav Kumar <ssauravkr1111@gmail.com>
2026-06-30 20:38:38 +0530

    Enhance Nova AI with connectivity check and music features
    Refactor Nova AI assistant by adding internet connectivity check and enhancing music playback functionality.

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ import time
 import uuid
 from websites import websites
 from greetings import greetings
+import random
 
-wake_words = ["nova", "nov"]
+wake_words = ["nova", "nov", "innova"]
 
 def aiProcess(command):
     with open("gemini_api.txt", "r") as f:
@@ -44,6 +45,12 @@ with open("news_api.txt", "r") as f:
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
+def is_connected():
+    try:
+        requests.get("https://www.google.com", timeout=3)
+        return True
+    except:
+        return False
 
 def speak_old(text):
     engine.say(text)
@@ -84,12 +91,13 @@ def play_audio(filename):
     time.sleep(0.05)
 
 def processCommand(c):
+    global active
     #print("processCommand:", repr(c))
     print(f"Command: {c}")
 
 
     if "open" in c.lower():
-
+        
         search_query = c.lower().replace("open", "").strip()
         site = search_query.replace(" ", "").replace(".com", "").replace(".in", "").strip()
         if site in websites:
@@ -102,58 +110,150 @@ def processCommand(c):
         speak(f"Opening {search_query}.")
 
     elif "search" in c.lower():
+        if not is_connected():
+            print("No internet connection.")
+            speak("No internet connection.")
+            return
         search_query = c.lower().replace("search", "").strip()
         url = f"https://www.google.com/search?q={search_query}"
         webbrowser.open(url)
         print(f"Searching for {search_query} on Google.")
         speak(f"Searching for {search_query} on Google.")
 
+    elif any(x in c.lower() for x in ["music","some music", "play anything"]):
+
+        name, url = random.choice(list(musiclibrary.music.items()))
+
+        webbrowser.open(url)
+        print(f"Playing {name}")
+        speak(f"Playing {name}")
+
     elif "play" in c.lower():
+        if not is_connected():
+            print("No internet connection.")
+            speak("No internet connection.")
+            return
         song = c.lower().replace("play", "").strip()
-        if song in musiclibrary.music:
-            url = musiclibrary.music[song]
-            webbrowser.open(url)
-            print(f"Playing {song}.")
-            speak(f"Playing {song}.")
+
+        for name, url in musiclibrary.music.items():
+            if name in song:
+                webbrowser.open(url)
+                print(f"Playing {name}")
+                speak(f"Playing {name}")
+                return
+        webbrowser.open(f"https://open.spotify.com/search/{song.replace(' ', '%20')}")
+        print(f"searching {song} on spotify")
+        speak(f"searching {song} on spotify")
+        #speak("Sorry, I couldn't find that song.")
 
     elif "news" in c.lower():
-        #print("News block entered")
-        r = requests.get(f"https://newsapi.org/v2/everything?q=india&language=en&sortBy=publishedAt&apiKey={news_api}", timeout=10)
-        #print("Status Code:", r.status_code)
+        if not is_connected():
+            print("No internet connection.")
+            speak("No internet connection.")
+            return
         
-        if r.status_code == 200:
-            #parse the JSON response
-            data = r.json()
+        response = requests.get(
+            f"https://newsapi.org/v2/everything?q=india&language=en&sortBy=publishedAt&apiKey={news_api}",
+            timeout=10
+        )
 
-            #Extract the articles from the response
+        if response.status_code == 200:
+
+            data = response.json()
             articles = data.get("articles", [])
-            print("Total Articles:", len(articles))
-            #Speak the headlines of the articles
+
+            if not articles:
+                print("No news found.")
+                speak("No news found.")
+                return
+            i = 0
             print("Press 'q' to stop the news...")
             speak("Press 'q' to stop the news...")
-            i = 1
-            for article in articles:
-                
-                if msvcrt.kbhit():
-                    key = msvcrt.getch().decode(errors="ignore").lower()
+            while i < len(articles):
 
-                    if key == "q":
-                        print("Stopping news...")
-                        speak("Stopping news")
+                #listen 5 news
+                for _ in range(5):
+                    if msvcrt.kbhit():
+                        key = msvcrt.getch().decode(errors="ignore").lower()
+
+                        if key == "q":
+
+                            print("Stopping news...")
+                            speak("Stopping news")
+                            break
+
+                    if i >= len(articles):
                         break
 
-                print(f"{i}. {article['title']}")
-                speak(article['title'])
-                i += 1
-    elif "close nova" in c.lower():
+                    print(f"{i+1}. {articles[i]['title']}")
+                    speak(articles[i]["title"])
+                    i += 1
+
+                # After all news finished
+                if i >= len(articles):
+                    print("That's all for today's news.")
+                    speak("That's all for today's news.")
+                    break
+
+                if key == "q":
+                    break
+                # Continue or not
+                print("Do you want me to continue? Please say yes or no.")
+                speak("Do you want me to continue? Please say yes or no.")
+
+                try:
+                    with sr.Microphone() as source:
+                        print("Listening...")
+                        audio = recognizer.listen(source, timeout=10, phrase_time_limit=3)
+
+                    reply = recognizer.recognize_google(audio).lower()
+                    print("Reply:", reply)
+
+                    # Continue
+                    if any(x in reply for x in ["yes", "continue", "go on", "sure", "ofcourse"]):
+                        continue
+
+                    # Stop news
+                    elif any(x in reply for x in ["no", "stop"]):
+                        print("Okay, stopping the news.")
+                        speak("Okay, stopping the news.")
+                        break
+
+                    # Disable Nova
+                    elif any(x in reply for x in ["exit", "quit", "stop", "deactivate", "sleep",]):
+                        print("Deactivating Nova...")
+                        speak("Deactivating Nova...")
+                        active = False
+                        return
+
+                    else:
+                        print("I will stop the news.")
+                        speak("I will stop the news.")
+                        break
+
+                except sr.WaitTimeoutError:
+                    print("No response received. Stopping the news.")
+                    speak("No response received. Stopping the news.")
+                    break
+
+                except sr.UnknownValueError:
+                    print("I didn't understand. Stopping the news.")
+                    speak("I didn't understand. Stopping the news.")
+                    break
+
+    elif any(word in c.lower() for word in ["close", "shutdown", "goodbye"]):
         print("Goodbye!")
         speak("Goodbye!")
         sys.exit()
         #other commands will attend by gemini
-    else:
-        response = aiProcess(c)
-        print(response)
-        speak(response)
+    # else:
+        # if not is_connected():
+        #     print("No internet connection.")
+        #     speak("No internet connection.")
+        #     return
+    #     response = aiProcess(c)
+    #     print(response)
+    #     speak(response)
 
 
     
@@ -161,9 +261,8 @@ def processCommand(c):
 if __name__ == "__main__":
     speak("Initializing Nova...")
 
-    r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=1)
+        recognizer.adjust_for_ambient_noise(source, duration=1)
     active = False
     while True:
         #Listen for the wake word "Nova"
@@ -173,12 +272,12 @@ if __name__ == "__main__":
             with sr.Microphone() as source:
                 if active:
                     print("Listening...")
-                    audio = r.listen(source, timeout=None, phrase_time_limit=5)
+                    audio = recognizer.listen(source, timeout=None, phrase_time_limit=5)
                 else:
                     print("Listening for wake word...")
-                    audio = r.listen(source, timeout=None, phrase_time_limit=3)
+                    audio = recognizer.listen(source, timeout=None, phrase_time_limit=3)
 
-            text = r.recognize_google(audio).lower()
+            text = recognizer.recognize_google(audio).lower()
             print(f"Heard: {text}")
 
             if not active:
@@ -186,9 +285,9 @@ if __name__ == "__main__":
                     active = True
                     command = text
                     for word in wake_words:
-                        command = command.replace(word, "")
+                        command = command.replace(word, "").strip()
 
-                    command = (command.replace("hey", "").strip())
+                    #command = (command.replace("hey", "").strip())
 
                     if command in greetings:
                         play_audio(greetings[command])
@@ -208,8 +307,8 @@ if __name__ == "__main__":
             if any(word in text for word in wake_words):
                 command = text
                 for word in wake_words:
-                    command = command.replace(word, "")
-                command = command.replace("hey", "").strip()
+                    command = command.replace(word, "").strip()
+                #command = command.replace("hey", "").strip()
 
                 
                 if command in greetings:
@@ -231,7 +330,6 @@ if __name__ == "__main__":
                 print("Deactivating Nova...")
                 speak("Deactivating Nova...")
                 continue
-
 
             processCommand(text)
 
